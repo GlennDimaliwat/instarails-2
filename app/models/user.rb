18ea384986d78f2b5bf6f42cddcb8e650df4d879
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-10-01 15:36:46 +1100

    Add a comment for the simple way to do top 10 trending users

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,8 +36,8 @@ class User < ApplicationRecord
   # Top 10 Trending Users
   scope :trending_users, ->{
     left_joins(:followers)
-    .group("users.id")
-    .order("count(followers.follower_id) DESC")
+    .group('users.id')
+    .order('count(followers.follower_id) DESC')
     .limit(10)
   }
 
